c127082549b4ff6f54863ef0df7bac13ccf2d50b
Mandeep Kumar Malwal <mandymalwal123@gmail.com>
2024-11-13 13:42:52 +0530

    Your commit message

---
+++ gittesting.js
@@ -0,0 +1,7 @@
+// Dummy JavaScript Code Example
+function greetUser() {
+    let userName = "John"; // Define a dummy variable
+    console.log("Hellooooo, " + userName + "!"); // Log a message to the console
+}
+
+greetUser(); // Call the function
